4f60cae06036d821325098fe7df772141ae2a0ea
Michael Schuld <mikeschuld@hazymindinteractive.com>
2015-02-05 21:58:16 -0700

    Working loot drops from tall lumpy evergreens. 20% chance to drop a worm with 2 logs

--- modmain.lua
+++ modmain.lua
@@ -10,3 +10,29 @@ Assets =
 }
 
 AddMinimapAtlas("minimap/wormhole_friendly.xml")
+
+local original_tall = nil
+
+local function vermicomposting_tall(inst)
+	original_tall.tall(inst)
+	
+	if(inst.build == "sparse" and math.random() < 0.2) then
+		inst.components.lootdropper:SetLoot({"worm_friendly", "log", "log"})
+	end
+end
+
+local function addtreeloot(inst)
+	if (original_tall == nil) then
+		original_tall = {}
+
+		for key, val in pairs(inst.components.growable.stages) do
+			if (val.name == "tall") then
+				original_tall.tall = val.fn
+				val.fn = vermicomposting_tall
+			end
+		end
+	end
+end
+
+AddPrefabPostInit("evergreen_sparse", addtreeloot)
+AddPrefabPostInit("evergreen_sparse_tall", addtreeloot)
